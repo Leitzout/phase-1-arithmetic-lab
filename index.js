1e4a44ace380cab58e609dee1ec0b594f0581724
Kaylee Marie Leitzel <leitz@Leitzout.localdomain>
2021-09-13 23:53:00 -0600

    commit tree c22be85f6c4e67e2b0af7504059b163cea38f5e4

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 1000);
+
+let num3 = 4;
+let num4 = 16;
+const mod = num3 % num4; 
+
+const max = Math.max(20);
